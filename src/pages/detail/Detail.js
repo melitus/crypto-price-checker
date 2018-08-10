743b6a55665ab9c7e2bbf3927adbc0c0141c0e42
Engr.Sunday <asmelitus@gmail.com>
2018-08-10 13:33:05 +0100

    added detail css file

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { API_URL } from '../../config';
 import { handleResponse, renderChangePercent } from '../../helpers.js';
 import Loading from '../common/Loading';
