1a2762b15d560269be30282c432e00fc6321309c
Engr.Sunday <asmelitus@gmail.com>
2018-08-10 12:15:50 +0100

    added images and header,search and loading components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Search from './Search';
+import logo from '../../assets/images/logo';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <Link to="/">
+        <img src={logo} alt="logo" className="Header-logo" />
+      </Link>
+
+      <Search />
+    </div>
+  );
+}
+
+export default Header;
