1aea898e2d96c2f0cec84e39f00134c75f0f79bd
Engr.Sunday <asmelitus@gmail.com>
2018-08-10 11:55:56 +0100

    removed some files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => {
+  return <h1>React Coin</h1>;
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
